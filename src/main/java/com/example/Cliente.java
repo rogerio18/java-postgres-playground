3b996e11b96aeb55f4d93319cd96e61c1a697376
rogerio18 silva <rogerdigadiga@gmail.com>
2023-08-22 03:53:10 +0000

    save4

--- src/main/java/com/example/Cliente.java
+++ src/main/java/com/example/Cliente.java
@@ -2,10 +2,40 @@ package com.example;
 
 public class Cliente{
     private String nome;
+    private String cpf;
     private double renda;
     private char sexo;
-    private int anoNascimento;    
+    private int anoNascimento; 
+    private boolean especial;   
 
+    // um contrutor é so assim, nao pode ter void, apenas public Cliente
+    public Cliente(){   
+        System.out.println("criando um cliente usando um construtor sem paramentros");
+        double aleatorio = Math.random();
+        if (aleatorio > 0.5){
+            especial = true;
+        }else especial = false;
+    }
+    public Cliente(String nome,double renda, char sexo){
+        this(); //chamando o contrutor acima pra rodar o random  
+        System.out.println("criando um cliente usando um contrutor com paramentros");
+        this.sexo = sexo;
+        setRenda(renda);
+        setNome(nome);;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public boolean isEspecial() {
+        return especial;
+    }//o metodo get para boleano e assim com esse is
+
+    public void setEspecial(boolean especial) {
+        this.especial = especial;
+    }
     public String getNome() {
         return nome;
     }
@@ -20,7 +50,11 @@ public class Cliente{
     }
 
     public void setNome(String nome) {
-        this.nome = nome;
+        this.nome = nome.toUpperCase().trim();
+        //touppercase ta convertendo para caixa alta
+        //tolowercase converteria para caixa baixa
+        //trim elimina espacos em branco
+
     }
     public void setRenda(double renda){
         if(renda >= 0)
@@ -28,7 +62,7 @@ public class Cliente{
         else System.out.println("a renda deve ser maior que zero");
     }
     public void setSexo(char sexo) {
-        if(sexo == 'M' || sexo == 'F')
+        if(sexo == 'M' || sexo == 'F'|| sexo == 'm' || sexo == 'f')
             this.sexo = sexo;
         else System.out.println("sexo invalido, informe M ou F");
     }
--- src/main/java/com/example/appclasses.java
+++ src/main/java/com/example/appclasses.java
@@ -1,6 +1,7 @@
 package com.example;
 
 public class appclasses {
+    
     public static void main(String[] args) {
         System.out.println();
         System.out.println("criando classes");
@@ -8,26 +9,42 @@ public class appclasses {
         
         Cliente cliente = new Cliente();
         cliente.setNome("rogerio");
-        cliente.setRenda(-2000);
+        cliente.setRenda(12000);
         cliente.setSexo('M');
         cliente.setAnoNascimento(1991);
+        cliente.setCpf("04642067310");
 
+        System.out.println("cpf: "+cliente.getCpf());
         System.out.println("o nome do cliente é "+cliente.getNome());
         System.out.println("a renda do cliente é "+cliente.getRenda());
         System.out.println("o sexo é "+cliente.getSexo());
         System.out.println("o ano de nascimento é "+cliente.getAnoNascimento());
+        System.out.println("especial: "+ cliente.isEspecial() );
         System.out.println();
-        Cliente cliente2 = new Cliente();
 
-        cliente2.setNome("isaias");
-        cliente2.setRenda(9600);
-        cliente2.setSexo('S'); 
-        cliente2.setAnoNascimento(1996);
+        Cliente cliente2 = new Cliente("isaias",9600, 'F');
+
+        cliente2.setCpf("046420673101");
+        //cliente2.setNome("isaias");
+        //cliente2.setRenda(9600);
+        //cliente2.setSexo('S'); 
+        //cliente2.setAnoNascimento(1996);
 
         System.out.println("o nome do cliente2 é "+cliente2.getNome());
+        System.out.println("cpf: "+cliente2.getCpf());
         System.out.println("a renda do cliente2 é "+cliente2.getRenda());
         System.out.println("o sexo é "+cliente2.getSexo());
         System.out.println("o ano de nascimento é "+cliente2.getAnoNascimento());
+        System.out.println("especial: "+ cliente2.isEspecial() );
+
+        //testando metodos strings
+        if(cliente.getCpf().equals(cliente2.getCpf())){
+            System.out.println("os clintes possuem o mesmo cpf");
+        }else {
+            System.out.println("os clientes possuem cpfs diferentes");
+        }
+        
+            
 
     }
 }
